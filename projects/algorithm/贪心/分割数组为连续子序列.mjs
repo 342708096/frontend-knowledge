def8dd89b7f85d1b0cd5c216b2c33ef59d7889a3
tonyzhu <qiuyuewu1987@163.com>
2022-05-25 15:12:09 +0800

    update

--- projects/algorithm/贪心/分割数组为连续子序列.mjs
+++ projects/algorithm/贪心/分割数组为连续子序列.mjs
@@ -41,28 +41,35 @@
  * @return {boolean}
  */
  var isPossible = function(nums) {
-    return process(nums, 0, nums.length - 1)
-};
-
-function process(nums, l, r) {
-    if (r - l + 1 < 3) {
-        return false
+    const countMap = new Map();
+    const endMap = new Map();
+    for (const x of nums) {
+        const count = (countMap.get(x) || 0) + 1;
+        countMap.set(x, count);
     }
-    let redundant = []
-    for (let i=l+1; i<=r; i++) {
-        if (nums[i] = nums[i-1]) {
-            redundant.push(nums[i])
-        } else if (num[i] > nums[i-1] + 1) {
-            if (redundant.length === 0) {
-                return process(nums, i, r)
+    for (const x of nums) {
+        const count = countMap.get(x) || 0;
+        if (count > 0) {
+            const prevEndCount = endMap.get(x - 1) || 0;
+            // 优先往前贪心
+            if (prevEndCount > 0) {
+                countMap.set(x, count - 1);
+                endMap.set(x - 1, prevEndCount - 1);
+                endMap.set(x, (endMap.get(x) || 0) + 1);
+            } else {
+                // 前面贪不成再考虑后面
+                const count1 = countMap.get(x + 1) || 0;
+                const count2 = countMap.get(x + 2) || 0;
+                if (count1 > 0 && count2 > 0) {
+                    countMap.set(x, count - 1);
+                    countMap.set(x + 1, count1 - 1);
+                    countMap.set(x + 2, count2 - 1);
+                    endMap.set(x + 2, (endMap.get(x + 2) || 0) + 1);
+                } else {
+                    return false;
+                }
             }
-            return process(redundant, 0, redundant.length-1) && process(nums, i, r)
         }
     }
-    if (redundant.length) {
-        return process(redundant, 0, redundant.length-1)
-    }
-    return true
-}
-
-console.log(isPossible([1,2,3,4,4,5]))
+    return true;
+};
