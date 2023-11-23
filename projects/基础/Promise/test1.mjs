70e80314fd3446a23de195dfe4ec943d979a7de7
zhuzheng <zhuzheng@xiaobing.ai>
2023-11-23 16:41:31 +0800

    feat: 更新

--- projects/基础/Promise/test1.mjs
+++ projects/基础/Promise/test1.mjs
@@ -1,3 +1,3 @@
-Promise.resolve(1).then(2).then(Promise.resolve(3)).finally(() => 4).then(console.log, console.log)  // 1
+Promise.resolve(1).then(2).then(Promise.resolve(3)).finally(() => 4).then(console.log, console.error)  // 1
 
-Promise.resolve(1).then(() => Promise.reject(2)).finally(() => Promise.reject(3)).then(console.log, console.log)// finally 如果reject 则相当于then, 否则返回上一个promise的resolve
+Promise.resolve(1).then(() => Promise.reject(2)).finally(() => Promise.reject(3)).then(console.log, console.error)// finally 如果reject 则相当于then, 否则返回上一个promise的resolve
